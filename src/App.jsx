3de7b9a5163074b98c0be0445cb3f3d0e53e3196
Rakib Mahmud <rakibas375@gmail.com>
2023-08-19 22:59:57 +0600

    react-cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,28 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import Profile from "./Component/Profile";
 
-
 function App() {
-// //declare a state 
-// const [users,setUsers]=useState([]);
+  const [users, setUsers] = useState([]);
 
-
-// //api call with axios
-// useEffect(()=>{
-//   fetch("https://jsonplaceholder.typicode.com/users")
-//   .then(res=>res.json())
-//   .then(data=>users(data))
-// },[]);
-const [users,setUsers]=useState([]);
-useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res=>res.json())
-    .then(data=>setUsers(data))
-},[]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((data) => {
+        // slice this api 
+        const get_user = data.slice(0, 50);
+        setUsers(get_user);
+      });
+  }, []);
 
   return (
     <div className="container">
       <div className="row">
-       
-        {/* {
-          users.map(user=>{
-            <Profile user={user}/>
-          })
-        } */}
-
-{
-                    users.map(user=><Profile user={user}/>)
-                }
+        {users.map((user) => (
+          <Profile key={user.id} user={user} />
+        ))}
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Component/Profile.jsx
+++ src/Component/Profile.jsx
@@ -2,18 +2,20 @@ import React from "react";
 
 
 export default function Profile(props){
-  console.log(props);
+  console.log(props.user);
+  const {title,thumbnailUrl}=props.user
     return (
         <div className="col-md-4 mb-4">
           <div className="card">
             <div className="card-header">
-                <h6>Welcome To Profile</h6>
+                <h6>{title}</h6>
             </div>
             <div className="card-body">
-                <h3>Hello</h3>
+               <img src={thumbnailUrl} alt="" />
             </div>
             <div className="card-footer">
               <button className="btn btn-primary">Add Friend</button>
+              
             </div>
           </div>
         </div>
